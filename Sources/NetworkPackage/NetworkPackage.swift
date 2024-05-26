5e7d7a29d31ca7c089f4db5724e47302d0c21312
tornike222 <tornike.elkanishvili.1@btu.edu.ge>
2024-05-26 13:12:34 +0400

    add error handlings

--- Sources/NetworkPackage/NetworkPackage.swift
+++ Sources/NetworkPackage/NetworkPackage.swift
@@ -5,52 +5,56 @@
 import Foundation
 
 public enum NetworkError: Error {
+    case invalidResponse
+    case httpError(code: Int)
+    case noData
     case decodeError
-    case wrongResponse
-    case wrongStatusCode(code: Int)
+    
 }
 //MARK: - Network Get Request
 
 public class NetworkService {
     
-    public init() {
-        }
-        
-    public func getData<T: Decodable>(urlString: String, completion: @escaping (T?,Error?) ->(Void)) {
-        var tryCount = 0
+    public init() { }
+    
+    public func getData<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> Void) {
         let url = URL(string: urlString)!
-        if tryCount <= 3 {
-            URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-                
-                if let error {
-                    print(error.localizedDescription)
-                }
-                
-                guard let response = response as? HTTPURLResponse else {
-                    return
-                }
-                
-                guard (200...299).contains(response.statusCode) else {
-                    print("wrong response")
-                    return
-                }
-                
-                guard let data else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Network error: \(error.localizedDescription)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                let invalidResponseError = NetworkError.invalidResponse
+                print("Invalid response")
+                completion(nil, invalidResponseError)
+                return
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                let httpError = NetworkError.httpError(code: httpResponse.statusCode)
+                print("HTTP error: \(httpResponse.statusCode)")
+                completion(nil, httpError)
+                return
+            }
+            
+            guard let data = data else {
+                let noDataError = NetworkError.noData
+                print("No data")
+                completion(nil, noDataError)
+                return
+            }
 
-                do {
-                    let decoder = JSONDecoder()
-                    let object = try decoder.decode(T.self, from: data)
-                                    
-                    DispatchQueue.main.async {
-                        completion(object, nil)
-                    }
-                } catch {
-                    print("Error decoding data:", error)
-                    completion(nil, NetworkError.decodeError)
-                    tryCount += 1
-                }
-            }.resume()
-        }
-        
+            do {
+                let decoder = JSONDecoder()
+                let object = try decoder.decode(T.self, from: data)
+                completion(object, nil)
+            } catch {
+                print("Error decoding data:", error)
+                completion(nil, NetworkError.decodeError)
+            }
+        }.resume()
     }
 }
