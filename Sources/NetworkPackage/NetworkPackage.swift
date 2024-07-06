c42699c2bf1c57875e2d4250bc099e155cfa93a6
tornike222 <tornike.elkanishvili.1@btu.edu.ge>
2024-07-06 15:11:05 +0400

    add bugfix of decodable

--- Sources/NetworkPackage/NetworkPackage.swift
+++ Sources/NetworkPackage/NetworkPackage.swift
@@ -8,7 +8,6 @@
 //
 import Foundation
 
-// Enum for Network Errors
 public enum NetworkError: Error {
     case invalidResponse
     case httpError(code: Int)
@@ -16,7 +15,6 @@ public enum NetworkError: Error {
     case decodeError
 }
 
-// Enum for HTTP Methods
 public enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
@@ -25,12 +23,10 @@ public enum HTTPMethod: String {
     case patch = "PATCH"
 }
 
-// NetworkService class for making requests
 public class NetworkService {
 
     public init() { }
-    
-    // Function to perform a generic HTTP request
+
     public func requestData<T: Decodable>(
         urlString: String,
         method: HTTPMethod = .get,
@@ -39,89 +35,67 @@ public class NetworkService {
         completion: @escaping (Result<T, Error>) -> Void
     ) {
         guard let url = URL(string: urlString) else {
-            print("Invalid URL")
             completion(.failure(NetworkError.invalidResponse))
             return
         }
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
-        
+
         if let headers = headers {
             for (key, value) in headers {
                 request.addValue(value, forHTTPHeaderField: key)
             }
         }
-        
+
         if let body = body {
             request.httpBody = body
         }
-        
+
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
-                print("Network error: \(error.localizedDescription)")
                 completion(.failure(error))
                 return
             }
-            
+
             guard let httpResponse = response as? HTTPURLResponse else {
-                let invalidResponseError = NetworkError.invalidResponse
-                print("Invalid response")
-                completion(.failure(invalidResponseError))
+                completion(.failure(NetworkError.invalidResponse))
                 return
             }
-            
+
             guard (200...299).contains(httpResponse.statusCode) else {
-                let httpError = NetworkError.httpError(code: httpResponse.statusCode)
-                print("HTTP error: \(httpResponse.statusCode)")
-                completion(.failure(httpError))
+                completion(.failure(NetworkError.httpError(code: httpResponse.statusCode)))
                 return
             }
-            
-            // Handle POST request without expecting data in the response
+
             if method == .post {
-                // Check if T is Void, in which case we return success with Void
-                if T.self == Void.self {
-                    DispatchQueue.main.async {
-                        completion(.success(() as! T)) // Cast to T, which is Void
-                    }
+                if T.self == EmptyResponse.self {
+                    completion(.success(EmptyResponse() as! T))
                 } else {
-                    // For other types, attempt to decode the response data
                     guard let data = data else {
-                        let noDataError = NetworkError.noData
-                        print("No data")
-                        completion(.failure(noDataError))
+                        completion(.failure(NetworkError.noData))
                         return
                     }
-                    
+
                     do {
                         let decoder = JSONDecoder()
                         let object = try decoder.decode(T.self, from: data)
-                        DispatchQueue.main.async {
-                            completion(.success(object))
-                        }
+                        completion(.success(object))
                     } catch {
-                        print("Error decoding data:", error)
                         completion(.failure(NetworkError.decodeError))
                     }
                 }
             } else {
-                // Handle other HTTP methods or scenarios where data is expected
                 guard let data = data else {
-                    let noDataError = NetworkError.noData
-                    print("No data")
-                    completion(.failure(noDataError))
+                    completion(.failure(NetworkError.noData))
                     return
                 }
-                
+
                 do {
                     let decoder = JSONDecoder()
                     let object = try decoder.decode(T.self, from: data)
-                    DispatchQueue.main.async {
-                        completion(.success(object))
-                    }
+                    completion(.success(object))
                 } catch {
-                    print("Error decoding data:", error)
                     completion(.failure(NetworkError.decodeError))
                 }
             }
